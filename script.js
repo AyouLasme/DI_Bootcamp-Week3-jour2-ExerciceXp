daa720e0048e67ef3d4fd6ecb271055c030d964d
Ayou Lasme <ayou.lasme@mail.com>
2023-01-04 22:27:55 +0000

    Exercice Xp Week3 jour2

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * @author: LASME Ayou Emeline 
+ */
+
+/******************************** Exercice1: Debut ********************************/
+
+// À l'aide d'une propriété DOM, récupérez le h1et console.log.
+let h1 = document.querySelectorAll("h1")
+h1.classList.add("titre")
+console.log(titre)
